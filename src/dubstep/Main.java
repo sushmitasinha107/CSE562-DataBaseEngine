c9bd3f5ee408140752c58b688c8efc04c41de159
AC <AC@Deeptis-MacBook-Air.local>
2017-03-03 15:56:43 -0500

    completed all clauses-I

--- src/dubstep/Main.java
+++ src/dubstep/Main.java
@@ -15,6 +15,7 @@ import net.sf.jsqlparser.eval.Eval;
 import net.sf.jsqlparser.expression.DateValue;
 import net.sf.jsqlparser.expression.DoubleValue;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.Function;
 import net.sf.jsqlparser.expression.LongValue;
 import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.expression.StringValue;
@@ -34,9 +35,24 @@ import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 import net.sf.jsqlparser.statement.select.SelectItem;
-import net.sf.jsqlparser.statement.select.SelectItemVisitor;
 
 public class Main {
+	
+	public static long aggAnswerL = 0;
+	public static double aggAnswerD = 0.0;
+	public static long minL = Integer.MAX_VALUE;
+	public static double minD = Integer.MAX_VALUE;
+	public static long maxL = Integer.MIN_VALUE;
+	public static double maxD = Integer.MIN_VALUE;
+	public static long count = 0;
+	public static double aggAvg = 0.0;
+	public static double total = 0.0;
+	public static long avgCount = 0;
+	public static double avg = 0.0;
+	public static Boolean aggPrint = false;
+	
+	public static List<String> aggAnswers = new ArrayList<String>();
+	public static HashMap<String, String> aggAnswersMap= new HashMap<String, String>();
 	
 	private class TableData{
 		Map<String, Integer> columnOrderMapping;
@@ -104,35 +120,23 @@ public class Main {
 					tableMapping.put(myTableName, tableData);
 					
 				} else if (query instanceof Select) {
-					
-					//System.out.println("select for table:  " + myTableName);
-					
-					Select select = (Select) query;
-					
-					
-					
+										
+					Select select = (Select) query;	
 					PlainSelect plainSelect = (PlainSelect) select.getSelectBody();
 					
-					myTableName = plainSelect.getFromItem().toString();
-					//System.out.println("select cha table name: " + myTableName);
-					
+					myTableName = plainSelect.getFromItem().toString();					
 					tableData = tableMapping.get(myTableName);
 					columnOrderMapping = tableData.getColumnOrderMapping();
 					columnDataTypeMapping = tableData.getColumnDataTypeMapping();
 					
 					List<String> selectItemsAsString = new ArrayList<String>();
-					List<SelectItem> selectItemsAsObject = new ArrayList<SelectItem>();
-
-					//System.out.println("this table's mapping: " + columnDataTypeMapping + "\n" + columnOrderMapping);
-					
+					List<SelectItem> selectItemsAsObject = new ArrayList<SelectItem>();					
 					
 					for (SelectItem sitem : plainSelect.getSelectItems()) {
 						selectItemsAsString.add(sitem.toString());
 						selectItemsAsObject.add(sitem);
 					}
-
 					
-
 					if (selectItemsAsString.get(0).toString().equals("*")) {
 						int j = 0;
 						for (String s : columnOrderMapping.keySet()) {
@@ -146,16 +150,17 @@ public class Main {
 							j++;
 						}
 					}
-					// List<Integer> selectIndexes =
-					// getSelectIndexesfromMap(selectItemsAsString,
-					// columnOrderMapping);
+					
 					List<Integer> selectIndexes = null;
 
 					Expression e = plainSelect.getWhere();
-
+					aggAnswers = new ArrayList<String>();
+					aggAnswersMap = new HashMap<String, String>();
+					
 					readFromFile(myTableName, selectIndexes, selectItemsAsObject, columnOrderMapping,
 							columnDataTypeMapping, e);
-
+					
+					//printAggregate();
 				} else {
 					// System.out.println("Not of type select");
 				}
@@ -166,6 +171,22 @@ public class Main {
 		}
 
 	}
+
+//	private static void printAggregate() {
+//		
+//		System.out.println("list: " + aggAnswers);
+//		if (aggAnswers.size() != 0) {
+//			StringBuilder sb = new StringBuilder();
+//			for (int i = 0; i < aggAnswers.size(); i++) {
+//				sb.append(aggAnswers.get(i));
+//				if (i != aggAnswers.size() - 1)
+//					sb.append('|');
+//			}
+//
+//			System.out.println(sb);
+//			aggAnswers = new ArrayList<String>();
+//		}
+//	}
 
 	private static PrimitiveType getPrimitiveValue(ColDataType datatype) {
 
@@ -192,22 +213,39 @@ public class Main {
 		return null;
 	}
 
-	private static List<Integer> getSelectIndexesfromMap(List<String> items, Map<String, Integer> map) {
+//	private static List<Integer> getSelectIndexesfromMap(List<String> items, Map<String, Integer> map) {
+//
+//		List<Integer> index = new ArrayList<Integer>();
+//
+//		for (int i = 0; i < items.size(); i++) {
+//			int num = map.get(items.get(i));
+//			index.add(num);
+//		}
+//		return index;
+//	}
 
-		List<Integer> index = new ArrayList<Integer>();
-
-		for (int i = 0; i < items.size(); i++) {
-			int num = map.get(items.get(i));
-			index.add(num);
-		}
-		return index;
+	public static void reinitializeValues(){
+		aggAnswerL = 0;
+		aggAnswerD = 0.0;
+		count = 0;
+		minL = Integer.MAX_VALUE;
+		minD = Integer.MAX_VALUE;
+		maxL = Integer.MIN_VALUE;
+		maxD = Integer.MIN_VALUE;
+		aggAvg = 0.0;
+		total = 0.0;
+		avgCount = 0;
+		avg = 0.0;
 	}
-
+	
 	public static void readFromFile(String tableName, List<Integer> index, List<SelectItem> selectItemsAsObject,
 			Map<String, Integer> columnOrderMapping, Map<String, PrimitiveType> columnDataTypeMapping, Expression expr)
 			throws SQLException {
 		 File file = new File("data/" + tableName + ".csv");
 		//File file = new File(tableName + ".csv");
+		
+		reinitializeValues();
+		
 		try {
 			Scanner sc = new Scanner(file);
 			while (sc.hasNext()) {
@@ -215,9 +253,8 @@ public class Main {
 				/* read line from csv file */
 				String newRow = sc.nextLine();
 				/* values array have individual column values from the file */
-				String[] values = newRow.split("\\|"); // change to | for
-														// submission
-
+				String[] values = newRow.split("\\|"); 
+				
 				/* where clause evaluation */
 				Eval eval = new Eval() {
 					public PrimitiveValue eval(Column c) {
@@ -225,7 +262,6 @@ public class Main {
 						 * get this column's index mapping so that we can get
 						 * the value from the values array
 						 */
-						// System.out.println("column: " + c);
 						int idx = columnOrderMapping.get(c.toString());
 						/*
 						 * get this column's datatype so that we know what to
@@ -237,8 +273,7 @@ public class Main {
 					}
 				};
 
-				// System.out.println("expression: " + expr);
-
+				//System.out.println("where expr:" + expr);
 				if (!(expr == null)) {
 					PrimitiveValue ret = eval.eval(expr);
 					if ("TRUE".equals(ret.toString())) {
@@ -247,43 +282,163 @@ public class Main {
 				} else {
 					printToConsole(index, values, columnOrderMapping, columnDataTypeMapping, selectItemsAsObject);
 				}
-
-			}
+				
+			}/*done with file reading...if aggregate function, then print after this and not in printToConsole*/
+			
+			if(aggPrint)
+				printAggregateResult(selectItemsAsObject);
+			
+//			if(minL != Integer.MAX_VALUE){
+//				aggAnswers.add(minL+"");
+//			}
+//			if (minD != Integer.MAX_VALUE){
+//				aggAnswers.add(minD+"");
+//			}
+//			if(maxL != Integer.MIN_VALUE){
+//				aggAnswers.add(maxL+"");
+//			}
+//			if (maxD != Integer.MIN_VALUE){
+//				aggAnswers.add(maxD+"");
+//			}
+//			if(count != 0){
+//				aggAnswers.add(count+"");
+//			}
+//			if(aggAnswerL != 0){
+//				aggAnswers.add(aggAnswerL+"");
+//				//System.out.println(aggAnswerL);
+//			}
+//			if(aggAnswerD != 0.0){
+//				aggAnswers.add(aggAnswerD+"");
+//				//System.out.println(aggAnswerD);
+//			}
+//			if(total != 0.0){
+//				aggAnswers.add((total/avgCount) +"");
+//			}
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private static void printAggregateResult(List<SelectItem> selectItemsAsObject) {
+			
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < selectItemsAsObject.size(); i++) {
+
+			SelectExpressionItem sitem = (SelectExpressionItem) selectItemsAsObject.get(i);
+			Expression selExp = sitem.getExpression();
+			String ssitem = sitem.toString();
+			sb.append(aggAnswersMap.get(ssitem));
+			if(i != selectItemsAsObject.size()-1){
+				sb.append('|');
+			}
+		}
+		
+		System.out.println(sb);
+		
 	}
 
 	private static void printToConsole(List<Integer> index, String[] values, Map<String, Integer> columnOrderMapping,
 			Map<String, PrimitiveType> columnDataTypeMapping, List<SelectItem> selectItemsAsObject)
 			throws SQLException {
 
-		// System.out.println("for print\n" + columnOrderMapping + "\t" +
-		// selectItemsAsObject );
-
 		StringBuilder sbuilder = new StringBuilder();
 
+		Boolean print = true;
 		for (int i = 0; i < selectItemsAsObject.size(); i++) {
 
 			SelectExpressionItem sitem = (SelectExpressionItem) selectItemsAsObject.get(i);
-			// System.out.println(sitem.getExpression() instanceof Addition);
 			Expression selExp = sitem.getExpression();
 
-			if (selExp instanceof Addition || selExp instanceof Subtraction || selExp instanceof Multiplication
+			String ssitem = sitem.toString();
+			if(!aggAnswersMap.containsKey(ssitem)){
+				if(sitem.toString().contains("MIN") || sitem.toString().contains("min")){
+					aggAnswersMap.put(ssitem, Integer.MAX_VALUE+"");
+				}else{
+					aggAnswersMap.put(ssitem, "0");		
+				}
+			}
+			print = true;
+			aggPrint = false;
+			if(selExp instanceof Function){
+				
+				print = false;
+				aggPrint = true;
+				
+				Function aggregateFunction = (Function) selExp;
+				String aggName = aggregateFunction.getName();
+				
+				if ("COUNT".equalsIgnoreCase(aggName)) {
+					count = Long.parseLong(aggAnswersMap.get(ssitem));
+					count++;
+					aggAnswersMap.put(ssitem, count+"");
+				} else {
+
+					Expression aggExpr = aggregateFunction.getParameters().getExpressions().get(0);
+
+					PrimitiveValue answer = null;
+					answer = computeExpression(columnDataTypeMapping, columnOrderMapping, aggExpr, values);
+					if ("SUM".equalsIgnoreCase(aggName)) {
+						if (answer instanceof LongValue) {
+							aggAnswerL = Long.parseLong(aggAnswersMap.get(ssitem));
+							aggAnswerL += answer.toLong();
+							aggAnswersMap.put(ssitem, aggAnswerL+"");
+						} else if (answer instanceof DoubleValue) {
+							aggAnswerD = Double.parseDouble(aggAnswersMap.get(ssitem));
+							aggAnswerD += answer.toDouble();
+							aggAnswersMap.put(ssitem, aggAnswerD+"");
+						}
+					} else if ("MIN".equalsIgnoreCase(aggName)) {
+						if (answer instanceof LongValue) {
+							minL = Long.parseLong(aggAnswersMap.get(ssitem));
+							if (answer.toLong() < minL) {
+								minL = answer.toLong();
+								aggAnswersMap.put(ssitem, minL+"");
+							}
+						} else if (answer instanceof DoubleValue) {
+							minD = Double.parseDouble(aggAnswersMap.get(ssitem));
+							if (answer.toDouble() < minD) {
+								minD = answer.toDouble();
+								aggAnswersMap.put(ssitem, minD+"");
+							}
+						}
+					} else if ("MAX".equalsIgnoreCase(aggName)) {
+						if (answer instanceof LongValue) {
+							maxL = Long.parseLong(aggAnswersMap.get(ssitem));
+							if (answer.toLong() > maxL) {
+								maxL = answer.toLong();
+								aggAnswersMap.put(ssitem, maxL+"");
+							}
+						} else if (answer instanceof DoubleValue) {
+							maxD = Double.parseDouble(aggAnswersMap.get(ssitem));
+							if (answer.toDouble() > maxD) {
+								maxD = answer.toDouble();
+								aggAnswersMap.put(ssitem, maxD+"");
+							}
+						}
+					} else if("AVG".equalsIgnoreCase(aggName)){
+						avg = Double.parseDouble(aggAnswersMap.get(ssitem));
+						if (answer instanceof LongValue) {
+							total += answer.toLong();
+						} else if (answer instanceof DoubleValue) {
+							total += answer.toDouble();
+						}
+						avgCount++;
+						avg = total/avgCount;
+						aggAnswersMap.put(ssitem, avg+"");
+					}
+
+				}
+				//sbuilder.append(answer);
+			}
+
+			else if (selExp instanceof Addition || selExp instanceof Subtraction || selExp instanceof Multiplication
 					|| selExp instanceof Division) {
 
 				Eval eval = new Eval() {
 					public PrimitiveValue eval(Column c) {
-						/*
-						 * get this column's index mapping so that we can get
-						 * the value from the values array
-						 */
-						// System.out.println("column: " + c);
+						
 						int idx = columnOrderMapping.get(c.toString());
-						/*
-						 * get this column's datatype so that we know what to
-						 * return
-						 */
 						PrimitiveType ptype = columnDataTypeMapping.get(c.toString());
 
 						return getReturnType(ptype, values[idx]);
@@ -292,7 +447,6 @@ public class Main {
 
 				PrimitiveValue result = eval.eval(selExp);
 				sbuilder.append(result);
-				// System.out.println("addition result: " + result);
 
 			} else {
 				int idx = columnOrderMapping.get(sitem.toString());
@@ -303,12 +457,26 @@ public class Main {
 				sbuilder.append("|");
 		}
 
-		// for (int i = 0; i < index.size(); i++) {
-		// sbuilder.append(values[index.get(i)]);
-		// if (i != index.size() - 1)
-		// sbuilder.append("|");
-		// }
-		System.out.println(sbuilder.toString());
+		if(print){
+			System.out.println(sbuilder.toString());
+		}
+	}
+	
+	private static PrimitiveValue computeExpression(Map<String, PrimitiveType> columnDataTypeMapping, Map<String, Integer> columnOrderMapping, Expression expr, String[] values) throws SQLException{
+		
+		Eval eval = new Eval() {
+			public PrimitiveValue eval(Column c) {
+				
+				int idx = columnOrderMapping.get(c.toString());
+				PrimitiveType ptype = columnDataTypeMapping.get(c.toString());
+
+				return getReturnType(ptype, values[idx]);
+			}
+		};
+
+		PrimitiveValue result = eval.eval(expr);
+		
+		return result;
 	}
 
 	private static PrimitiveValue getReturnType(PrimitiveType ptype, String value) {
